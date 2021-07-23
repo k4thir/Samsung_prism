4a343eb89f726c28857c24f51d9674ad75395ad5
Kathir <kathirganesh23@gmail.com>
2021-07-23 11:34:02 +0530

    getUserMedia

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var video = document.querySelector("#videoElement");
 
 function start(e){
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
+  navigator.mediaDevices.getUserMedia ({ video: { width: 1080, framerate: 15 })
     .then(function (stream) {
       video.srcObject = stream;
     })
